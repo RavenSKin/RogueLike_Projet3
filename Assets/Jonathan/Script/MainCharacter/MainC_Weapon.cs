698ffdd96aa7a7307f3391dc8c8b37a107ed598d
unknown <cartman59165@gmail.com>
2021-01-25 21:26:04 +0100

    Add Weapon System

--- Assets/Jonathan/Script/MainCharacter/MainC_Weapon.cs
+++ Assets/Jonathan/Script/MainCharacter/MainC_Weapon.cs
@@ -1,14 +1,20 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MainC_Weapon : MonoBehaviour
 {
-
-
-    public string ActuallWeapon = "";
+    public string str_ActuallWeapon = "";
     public Transform WeaponPlace;
+    public int i_ActualAtt;
+    public int i_ActualAttSpeed;
+
+
+
     public bool b_CanAttaque;
+     //Définir quel est le genre d'attaque
+     public bool b_CaC;
+    public bool b_Dist;
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +25,12 @@ public class MainC_Weapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.K) && ActuallWeapon !="")
-        (
-            
-            ActuallWeapon == "";
-        )
+        if(Input.GetKeyDown(KeyCode.K) && str_ActuallWeapon !=null)
+        {
+            str_ActuallWeapon = "";
+        }
+
+        
+
     }
 }
--- Assets/Jonathan/Script/Weapon/Wpn_List.cs
+++ Assets/Jonathan/Script/Weapon/Wpn_List.cs
@@ -1,18 +1,49 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Wpn_List : MainC_Weapon
 {
 
-    public string SmallSword;
-    public int i_Stats_Attaque;
-    public int i_Stats_AttaqueSpeed;
 
-    public void Start() {
-        SmallSword ="Epee courte";
+    //Donner les statistiques pour l'arme "SmallSword"
+    public int i_SmallSword_Att;
+    public int i_SamllSword_AttSpeed;
+    public GameObject gm_BrokenSword;
+
+     //Donner les statistiques pour l'arme "Sword"
+    public int i_Sword_Att;
+    public int i_Sword_AttSpeed;
+    public GameObject gm_Sword;
+
+     //Donner les statistiques pour l'arme "HeavySword"
+    public int i_HeavySword_Att;
+    public int i_HeavySword_AttSpeed;
+    public GameObject gm_HeavySword;
+
+     //Donner les statistiques pour l'arme "Dague"
+    public int i_Dague_Att;
+    public int i_Dague_AttSpeed;
+    public GameObject gm_Dague;
+
+    public int i_doubleDague_Att;
+    public int i_doubleDague_AttSpeed;
+    public GameObject gm_DoubleDague;
+
+    public void Update() {
         
+        if(str_ActuallWeapon == "")
+        {
+            i_ActualAtt = 1;
+            i_ActualAttSpeed = 1;
+            gm_DoubleDague.SetActive(false);
+            gm_Dague.SetActive(false);
+            gm_HeavySword.SetActive(false);
+            gm_Sword.SetActive(false);
+            gm_BrokenSword.SetActive(false);
 
+            
+        }
     }
 
     public void OnTriggerEnter2D(Collider2D col) {
@@ -20,10 +51,58 @@ public class Wpn_List : MainC_Weapon
         //L'armes au sol devien mon arme
         //MainC_Weapon != null
         
-        if(col.gameObject.name == "Epee courte" && MyWeapon == "" && Input.GetKey(KeyCode.K))
-        {   
-            //MainC_Weapons.MyWeapon == Epee courte
+        
 
+        if(col.gameObject.name == "Sword" && str_ActuallWeapon == "")
+        {   
+            Debug.Log(col.gameObject.name);
+            i_ActualAtt = i_Sword_Att;
+            i_ActualAttSpeed = i_Sword_AttSpeed;
+            gm_Sword.SetActive(true);
+            gm_DoubleDague.SetActive(false);
+            gm_Dague.SetActive(false);
+            gm_HeavySword.SetActive(false);
+            gm_BrokenSword.SetActive(false);
+            str_ActuallWeapon = "Sword";
+        }
+
+        if(col.gameObject.name == "Dague" && str_ActuallWeapon == "")
+        {
+            Debug.Log(col.gameObject.name);
+            i_ActualAtt = i_Dague_Att;
+            i_ActualAttSpeed = i_Dague_AttSpeed;
+            gm_Dague.SetActive(true);
+            gm_DoubleDague.SetActive(false);
+            gm_HeavySword.SetActive(false);
+            gm_Sword.SetActive(false);
+            gm_BrokenSword.SetActive(false);
+            str_ActuallWeapon = "Dague";
+        }
+
+        if(col.gameObject.name == "HeavySword" && str_ActuallWeapon == "")
+        {
+            Debug.Log(col.gameObject.name);
+            i_ActualAtt =  i_HeavySword_Att;
+            i_ActualAttSpeed = i_HeavySword_AttSpeed;
+             gm_DoubleDague.SetActive(false);
+            gm_Dague.SetActive(false);
+            gm_HeavySword.SetActive(true);
+            gm_Sword.SetActive(false);
+            gm_BrokenSword.SetActive(false);
+            str_ActuallWeapon = "HeavySword";
+        }
+
+        if(col.gameObject.name == "BrokenSword" && str_ActuallWeapon == "")
+        {
+            Debug.Log(col.gameObject.name);
+            i_ActualAtt = i_SmallSword_Att;
+            i_ActualAttSpeed =i_SamllSword_AttSpeed;
+             gm_DoubleDague.SetActive(false);
+            gm_Dague.SetActive(false);
+            gm_HeavySword.SetActive(false);
+            gm_Sword.SetActive(false);
+            gm_BrokenSword.SetActive(true);
+            str_ActuallWeapon = "BrokenSword";
         }
 
     }
