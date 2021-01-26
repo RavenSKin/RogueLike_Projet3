c36b49242292dfa1400cde3c1af8797c964c5f26
Momouton <l.juste42@laposte.net>
2021-01-26 15:34:23 +0100

    add some new hud

---
+++ Assets/Louis/Scipts/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private UI_Inventory uiInventory;
+
+    private Inventory inventory;
+
+    private void Awake()
+    {
+        inventory = new Inventory();
+
+        uiInventory.SetInventory(inventory);
+    }
+}
