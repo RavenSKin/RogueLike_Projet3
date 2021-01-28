661a847a4844fa4b8da644ea1628049881fa9504
Momouton <l.juste42@laposte.net>
2021-01-28 17:09:32 +0100

    Louis/Feature/Shop

---
+++ Assets/Louis/Scipts/Coins.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coins : MonoBehaviour
+{
+    public int PieceDuJoueur; 
+    public GameObject Character;
+
+    public List<GameObject> _ItemSlot = new List<GameObject>();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PieceDuJoueur = Character.GetComponent<MainC_coins>().Coin;
+    }
+}
